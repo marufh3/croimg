7ac310f42554e2b5a90c4bffb012d487be32e291
marufh3 <mdmarufhasan3@gmail.com>
2025-11-02 23:35:03 +0600

    Update cropper build.gradle.kts for Java 17 and add jitpack.yml

--- cropper/build.gradle.kts
+++ cropper/build.gradle.kts
@@ -1,80 +1,79 @@
 plugins {
-  id("org.jetbrains.dokka")
-  id("org.jetbrains.kotlin.android")
-  id("com.android.library")
-  id("org.jetbrains.kotlin.plugin.parcelize")
-  id("com.vanniktech.maven.publish")
-  id("app.cash.licensee")
-  id("app.cash.paparazzi")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.parcelize")
+    id("org.jetbrains.dokka")
+    id("com.vanniktech.maven.publish")
+    id("app.cash.licensee")
+    id("app.cash.paparazzi")
 }
 
 licensee {
-  allow("Apache-2.0")
+    allow("Apache-2.0")
 }
 
+// 🟢 Kotlin JVM Toolchain - Java 17
 kotlin {
-  jvmToolchain {
-    languageVersion.set(JavaLanguageVersion.of(11))
-  }
-}
-
-android {
-  namespace = "com.canhub.cropper"
-
-  compileSdk = libs.versions.compileSdk.get().toInt()
-
-  defaultConfig {
-    minSdk = libs.versions.minSdk.get().toInt()
-  }
-
-  buildFeatures {
-    viewBinding = true
-  }
-
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-  }
-
-  testOptions {
-    unitTests.isIncludeAndroidResources = true
-  }
-}
-
-dependencies {
-  implementation(libs.androidx.activity.ktx)
-  implementation(libs.androidx.appcompat)
-  implementation(libs.androidx.core.ktx)
-  implementation(libs.androidx.exifinterface)
-  implementation(libs.kotlinx.coroutines.android)
-  implementation(libs.kotlinx.coroutines.core)
-}
-
-dependencies {
-  testImplementation(libs.androidx.fragment.testing)
-  testImplementation(libs.androidx.test.junit)
-  testImplementation(libs.junit)
-  testImplementation(libs.mock)
-  testImplementation(libs.robolectric)
-}
-
-// Workaround https://github.com/cashapp/paparazzi/issues/1231
-plugins.withId("app.cash.paparazzi") {
-  // Defer until afterEvaluate so that testImplementation is created by Android plugin.
-  afterEvaluate {
-    dependencies.constraints {
-      add("testImplementation", "com.google.guava:guava") {
-        attributes {
-          attribute(
-            TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
-            objects.named(TargetJvmEnvironment::class.java, TargetJvmEnvironment.STANDARD_JVM),
-          )
-        }
-        because(
-          "LayoutLib and sdk-common depend on Guava's -jre published variant." +
-            "See https://github.com/cashapp/paparazzi/issues/906.",
-        )
-      }
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
-  }
+}
+
+// 🟢 Android settings
+android {
+    namespace = "com.canhub.cropper"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 23
+        targetSdk = 35
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+    }
+}
+
+// 🟢 Dependencies (toml references)
+dependencies {
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.exifinterface)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+
+    testImplementation(libs.androidx.fragment.testing)
+    testImplementation(libs.androidx.test.junit)
+    testImplementation(libs.junit)
+    testImplementation(libs.mock)
+    testImplementation(libs.robolectric)
+}
+
+// 🟢 Paparazzi Guava workaround
+plugins.withId("app.cash.paparazzi") {
+    afterEvaluate {
+        dependencies.constraints {
+            add("testImplementation", "com.google.guava:guava") {
+                attributes {
+                    attribute(
+                        TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
+                        objects.named(TargetJvmEnvironment::class.java, TargetJvmEnvironment.STANDARD_JVM),
+                    )
+                }
+                because(
+                    "LayoutLib and sdk-common depend on Guava's -jre published variant. See https://github.com/cashapp/paparazzi/issues/906."
+                )
+            }
+        }
+    }
 }
